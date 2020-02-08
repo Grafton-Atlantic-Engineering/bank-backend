526af36767d0401371cd63fded31b6a2d562c9ae
Oleg Silkin <o.silkin98@gmail.com>
2020-02-08 12:45:21 -0500

    Moves src contents to package

--- src/TransactionEngine/Main.java
+++ src/TransactionEngine/Main.java
@@ -1,11 +1,10 @@
+package TransactionEngine;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /* TODO: create CSV reader class
 *   TODO: add essential field specification for csv parser
@@ -27,6 +26,7 @@ public class Main {
         while((row = csvReader.readLine()) != null) {
             // catch the empty strings
             dataRows.add(row.split(",", -1));
+
         }
         csvReader.close();
     }
